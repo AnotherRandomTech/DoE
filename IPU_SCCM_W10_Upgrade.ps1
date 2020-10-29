dd7a0eb40355231a486476956159a0f065ae3b24
Luke <31136246+AnotherRandomTech@users.noreply.github.com>
2020-10-29 09:58:13 +0800

    Added SCCM Windows 10 IPU v1
    SCCM in place upgrade script for Windows 10 machines.

---
+++ IPU_SCCM_W10_Upgrade.ps1
@@ -0,0 +1,8 @@
+﻿### Below gets all the programs in the SCCM Software Centre "Operating Systems" GUI
+# get-wmiobject -query "SELECT * FROM CCM_Program" -namespace "ROOT\ccm\ClientSDK" | Out-GridView
+
+## Installs Windows 10 Education [1909] using in place upgrade method.
+$IPUTask = (get-wmiobject -query "SELECT * FROM CCM_Program" -namespace "ROOT\ccm\ClientSDK") | Where-Object { $_.FullName -like "*IPU: Windows 10 Education*" }
+
+## ReturnValue should be 0 if it has started the upgrade sequence.
+Invoke-WmiMethod -Class CCM_ProgramsManager -Namespace "root\ccm\clientsdk" -Name ExecutePrograms -argumentlist $IPUTask
